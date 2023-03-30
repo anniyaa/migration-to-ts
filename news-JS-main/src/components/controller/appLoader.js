89da6d65371e183d6ce6a65990ce1478818bd28e
Anna Vi <nibiruru@yandex.ru>
2023-03-30 12:35:19 +0300

    feat: add api key

--- news-JS-main/src/components/controller/appLoader.js
+++ news-JS-main/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '3ac23bc4f9db4737a5dd3b23d9d0206f', // получите свой ключ https://newsapi.org/
         });
     }
 }
